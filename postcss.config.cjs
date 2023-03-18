c39f49cdb7ac494cf94109a14db2c782934a8c62
kevinhws <kevinshu1995@gmail.com>
2023-03-18 23:37:23 +0800

    fix: 🐛 tailwind preflight css prefix

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -4,6 +4,12 @@ module.exports = {
         autoprefixer: {},
         "postcss-prefix-selector": {
             prefix: "#dictionary-extension-app-container", // you can change this whatever you want
+            transform: function (prefix, selector, prefixedSelector, filePath, rule) {
+                if (selector === "#dictionary-extension-app-container") {
+                    return selector;
+                }
+                return prefixedSelector;
+            },
         },
         "tailwindcss/nesting": {},
     },
