aa3ebe51586c97be5ac635d5cdd308315b75d19a
kevinhws <kevinshu1995@gmail.com>
2023-03-18 16:14:16 +0800

    feat: 🎸 first commit

---
+++ lib/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const isDev = import.meta.env.DEV;
+</script>
+
+<template>
+    <div v-if="isDev">
+        <section>Testing section</section>
+    </div>
+</template>
+
+<style scoped></style>
+
